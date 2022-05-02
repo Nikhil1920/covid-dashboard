65f8dcb771cd73859cbb740b87a041d922e62d67
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2022-05-02 19:46:31 +0530

    add world data type

---
+++ frontend/types/WorldDataResponseType.ts
@@ -0,0 +1,11 @@
+type WorldDataResponseType = {
+    NewConfirmed: number;
+    TotalConfirmed: number;
+    NewDeaths: number;
+    TotalDeaths: number;
+    NewRecovered: number;
+    TotalRecovered: number;
+    Date: Date;
+};
+
+export default WorldDataResponseType;
